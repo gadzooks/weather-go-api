51908b2f066b12eb31398334508319dca78acb49
Amit Karwande <karwande@gmail.com>
2020-08-09 11:57:48 -0700

    use zerolog

--- go.mod
+++ go.mod
@@ -4,9 +4,12 @@ go 1.14
 
 require (
 	github.com/golang/mock v1.4.3
+	github.com/google/uuid v1.1.1
 	github.com/gorilla/mux v1.7.4
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/rs/cors v1.7.0
+	github.com/rs/zerolog v1.19.0
+	github.com/sirupsen/logrus v1.4.2
 	go.mongodb.org/mongo-driver v1.4.0
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.3.0
--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"context"
 	"fmt"
 	"github.com/gadzooks/weather-go-api/middleware"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
-	"log"
 	"net/http"
 	"os"
 	"time"
@@ -18,8 +19,16 @@ import (
 const MongoTimeOutInSeconds = 10
 
 func main() {
+	// UNIX Time is faster and smaller than most timestamps
+	// If you set zerolog.TimeFieldFormat to an empty string,
+	// logs will write with UNIX time
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+
 	// show filename and line number in logs
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.Logger = log.With().Caller().Logger()
+	//log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	log.Logger = log.With().Str("requestId", "bar").Logger()
 
 	ctx, cancel := context.WithTimeout(context.Background(), MongoTimeOutInSeconds*time.Second)
 	defer cancel()
@@ -34,10 +43,10 @@ func main() {
 	handler := middleware.WithCors(r)
 	handler = middleware.WithResponseTimeLogging(handler)
 
-	log.Println("starting server at 8080")
+	log.Info().Msg("starting server at 8080")
 	err := http.ListenAndServe(":8080", handler)
 	if err != nil {
-		log.Fatalf("error running server : %v", err)
+		log.Fatal().Msgf("error running server : %v", err)
 	}
 
 }
@@ -52,16 +61,16 @@ func mongoConnect(ctx context.Context) *mongo.Client {
 		mongoPass,
 		mongoDB,
 	)
-	log.Println("connecting via : ", uri)
+	log.Info().Msgf("connecting via : ", uri)
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Msg(err.Error())
 	}
 	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal().Msg(err.Error())
 	}
-	log.Println("connected to mongo successfully !")
+	log.Info().Msg("connected to mongo successfully !")
 
 	return client
 }
